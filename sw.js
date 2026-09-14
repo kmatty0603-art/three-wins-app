788c94b4d78444a0d2840ef7262b321ccf1e4c73
kmatty0603-art <k.matty0603@gmail.com>
2026-09-15 00:56:27 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-const CACHE='three-wins-v1';
-const ASSETS=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+const C='three-wins-v2';const A=['./','index.html','manifest.json','icon-192.png','icon-512.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
